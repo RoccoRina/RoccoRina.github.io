f846d12b31169f3f067d1949235c3ac9c236ac8e
RoccoRina <43075138+RoccoRina@users.noreply.github.com>
2018-12-08 01:46:33 +0300

    Add files via upload

---
+++ MODUL/js/index.js
@@ -0,0 +1,36 @@
+var elements = $('.modal-overlay1, .modal1');
+
+$('.firstSound').click(function(){
+    elements.addClass('active');
+});
+
+$('.close-modal').click(function(){
+    elements.removeClass('active');
+});
+var elements2 = $('.modal-overlay2, .modal2');
+
+$('.secondSound').click(function(){
+    elements2.addClass('active');
+});
+
+$('.close-modal').click(function(){
+    elements2.removeClass('active');
+});
+var elements3 = $('.modal-overlay3, .modal3');
+
+$('.thirdSound').click(function(){
+    elements3.addClass('active');
+});
+
+$('.close-modal').click(function(){
+    elements3.removeClass('active');
+});
+var elements4 = $('.modal-overlay4, .modal4');
+
+$('.fourthSound').click(function(){
+    elements4.addClass('active');
+});
+
+$('.close-modal').click(function(){
+    elements4.removeClass('active');
+});
